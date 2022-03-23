2f0725d1c0093c5e52777fe5440f473e96ad8e1d
Adam Dumas <misterinevitable@outlook.com>
2022-03-23 14:46:47 -0700

    Changed source order of contents of ForwardList

--- src/DataStructures/LinkedList/ForwardList.php
+++ src/DataStructures/LinkedList/ForwardList.php
@@ -6,66 +6,6 @@ namespace DataStructures\LinkedList;
 // next node in the list.
 class ForwardList
 {
-    private $head = null;
-
-    public function is_empty()
-    {
-        return is_null($this->head);
-    }
-
-    public function append($item)
-    {
-        $node = new ForwardNode($item);
-
-        if (is_null($this->head))
-            $this->head = $node;
-        else
-            $this->last_node()->next = $node;
-    }
-
-    private function last_node()
-    {
-        if (is_null($this->head))
-            return null;
-
-        $last = $this->head;
-        $next = $last->next;
-
-        while (!is_null($next)) {
-            $last = $next;
-            $next = $last->next;
-        }
-
-        return $last;
-    }
-
-    public function prepend($item)
-    {
-        $this->head = new ForwardNode($item, $this->head);
-    }
-
-    public function items()
-    {
-        for ($node = $this->head; !is_null($node); $node = $node->next)
-            yield $node->data; // PHP also makes a sequential key available to caller
-    }
-
-    public function first()
-    {
-        if (is_null($this->head))
-            throw new InvalidOperationException('no first item');
-
-        return $this->head->data;
-    }
-
-    public function last()
-    {
-        if (is_null($this->head))
-            throw new InvalidOperationException('no last item');
-
-        return $this->last_node()->data;
-    }
-
     public static function from_array($arr)
     {
         $node = null;
@@ -78,13 +18,15 @@ class ForwardList
         
         return $list;
     }
-
-    public function to_array()
+    
+    public function append($item)
     {
-        $arr = [];
-        foreach ($this->items() as $item)
-            $arr[] = $item;
-        return $arr;
+        $node = new ForwardNode($item);
+
+        if (is_null($this->head))
+            $this->head = $node;
+        else
+            $this->last_node()->next = $node;
     }
 
     public function count() : int
@@ -115,44 +57,46 @@ class ForwardList
         return $this->count_if($equals_item);
     }
 
-    public function remove_one($item) : mixed
+    public function first()
     {
-        // So we don't need to treat the head node as special, we add one.
-        $faux_head = new ForwardNode(null, $this->head);
+        if (is_null($this->head))
+            throw new InvalidOperationException('no first item');
 
-        $before = $this->find_node_before($item, $faux_head);
-
-        if (is_null($before)) {
-            return null;
-        }
-
-        $data = $before->next->data;
-        $before->next = $before->next->next;
-        
-        // Update head since the previous one may have been removed.
-        $this->head = $faux_head->next;
-
-        return $data;
+        return $this->head->data;
     }
 
-    // Find the first node after $before which has $item and return the node
-    // before it
-    private function find_node_before($item, ?ForwardNode $before) : ?ForwardNode
+    public function insert_after($needle, $item) : bool
     {
-        // $before is the first possible node we could return
-        if (is_null($before) || is_null($before->next))
-            return null;
+        return false; // TODO
+    }
 
-        $before = $before;
+    public function insert_before($needle, $item) : bool
+    {
+        return false; // TODO
+    }
 
-        for ($node = $before->next; !is_null($node); $node = $node->next) {
-            if ($node->data === $item)
-                return $before;
+    public function is_empty()
+    {
+        return is_null($this->head);
+    }
 
-            $before = $node;
-        }
+    public function items()
+    {
+        for ($node = $this->head; !is_null($node); $node = $node->next)
+            yield $node->data; // PHP also makes a sequential key available to caller
+    }
 
-        return null;
+    public function last()
+    {
+        if (is_null($this->head))
+            throw new InvalidOperationException('no last item');
+
+        return $this->last_node()->data;
+    }
+
+    public function prepend($item)
+    {
+        $this->head = new ForwardNode($item, $this->head);
     }
 
     public function remove_all($item) : int
@@ -178,13 +122,69 @@ class ForwardList
         return $count;
     }
 
-    public function insert_before($needle, $item) : bool
+    public function remove_one($item) : mixed
     {
-        return false; // TODO
+        // So we don't need to treat the head node as special, we add one.
+        $faux_head = new ForwardNode(null, $this->head);
+
+        $before = $this->find_node_before($item, $faux_head);
+
+        if (is_null($before)) {
+            return null;
+        }
+
+        $data = $before->next->data;
+        $before->next = $before->next->next;
+        
+        // Update head since the previous one may have been removed.
+        $this->head = $faux_head->next;
+
+        return $data;
     }
 
-    public function insert_after($needle, $item) : bool
+    public function to_array()
     {
-        return false; // TODO
+        $arr = [];
+        foreach ($this->items() as $item)
+            $arr[] = $item;
+        return $arr;
     }
+
+    // Find the first node after $before which has $item and return the node
+    // before it
+    private function find_node_before($item, ?ForwardNode $before) : ?ForwardNode
+    {
+        // $before is the first possible node we could return
+        if (is_null($before) || is_null($before->next))
+            return null;
+
+        $before = $before;
+
+        for ($node = $before->next; !is_null($node); $node = $node->next) {
+            if ($node->data === $item)
+                return $before;
+
+            $before = $node;
+        }
+
+        return null;
+    }
+
+    private function last_node()
+    {
+        if (is_null($this->head))
+            return null;
+
+        $last = $this->head;
+        $next = $last->next;
+
+        while (!is_null($next)) {
+            $last = $next;
+            $next = $last->next;
+        }
+
+        return $last;
+    }
+
+    private $head = null;
 }
